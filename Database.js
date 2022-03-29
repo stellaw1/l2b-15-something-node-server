4bd227174715bdd0b5ec9ba3e4ba0e041a3429c5
stellaw1 <stella01wang@gmail.com>
2022-03-29 15:12:29 -0700

    init db code

---
+++ Database.js
@@ -0,0 +1,143 @@
+const { MongoClient, AbstractCursor } = require('mongodb');	// require the mongodb driver
+
+/**
+ * Uses mongodb v3.6+ - [API Documentation](http://mongodb.github.io/node-mongodb-native/3.6/api/)
+ * Database wraps a mongoDB connection to provide a higher-level abstraction layer
+ * for manipulating the objects in our cpen322 app.
+ */
+function Database(mongoUrl, dbName){
+	if (!(this instanceof Database)) return new Database(mongoUrl, dbName);
+	this.connected = new Promise((resolve, reject) => {
+		MongoClient.connect(
+			mongoUrl,
+			{
+				useNewUrlParser: true
+			},
+			(err, client) => {
+				if (err) reject(err);
+				else {
+					console.log('[MongoClient] Connected to ' + mongoUrl + '/' + dbName);
+					resolve(client.db(dbName));
+				}
+			}
+		)
+	});
+	this.status = () => this.connected.then(
+		db => ({ error: null, url: mongoUrl, db: dbName }),
+		err => ({ error: err })
+	);
+}
+
+Database.prototype.getUsers = function(){
+	return this.connected.then(db =>
+		new Promise((resolve, reject) => {
+			/* TODO: read the chatrooms from `db`
+			 * and resolve an array of chatrooms */
+			const col = db.collection('users');
+			col.find({}).toArray(function(err, items) {
+
+					if (err) {
+						reject(err);
+					}
+
+					resolve(items);
+				});
+		})
+	)
+}
+
+function isActivityType(activity) {
+	if (!activity.hasOwnProperty("id") || !Number.isInteger(activity["id"])) {
+		return "id";
+	}
+	if (!activity.hasOwnProperty("date") || typeof(activity["date"]) != "string") {
+		return "date";
+	}
+	if (!activity.hasOwnProperty("name") || typeof(activity["name"]) != "string") {
+		return "name";
+	}
+	if (!activity.hasOwnProperty("duration") || !Number.isInteger(activity["duration"])) {
+		return "duration";
+	}
+	if (!activity.hasOwnProperty("distance") || typeof(activity["distance"]) != "number") {
+		return "distance";
+	}
+	return null;
+}
+
+Database.prototype.postActivity = function(activity){
+	return this.connected.then(db =>
+		new Promise((resolve, reject) => {
+
+			let err;
+			if (err = isActivityType(activity)) {
+				reject(new Error("invalid " + err + " property in given activity"));
+			}
+
+			const col = db.collection('activities');
+				col.insertOne(activity, function(err, res){
+					if(err){
+						console.log(err);
+					}
+					resolve(activity);
+				});
+		})
+	)
+}
+
+function isPutActivityType(activity) {
+	if (!activity.hasOwnProperty("date") || typeof(activity["date"]) != "string") {
+		return "date";
+	}
+	if (!activity.hasOwnProperty("name") || typeof(activity["name"]) != "string") {
+		return "name";
+	}
+	if (!activity.hasOwnProperty("duration") || !Number.isInteger(activity["duration"])) {
+		return "duration";
+	}
+	if (!activity.hasOwnProperty("distance") || typeof(activity["distance"]) != "number") {
+		return "distance";
+	}
+	return null;
+}
+
+Database.prototype.putActivity = function(id, activity){
+	return this.connected.then(db =>
+		new Promise((resolve, reject) => {
+
+			let err;
+			if (err = isPutActivityType(activity)) {
+				reject(new Error("invalid " + err + " property in given activity"));
+			}
+
+			var myquery = { id: id };
+  			var newvalues = { $set: activity };
+
+			const col = db.collection('activities');
+				col.updateOne(myquery, newvalues, function(err, res){
+					if(err){
+						console.log(err);
+					}
+					resolve(res);
+				});
+		})
+	)
+}
+
+Database.prototype.deleteActivity = function(id){
+	return this.connected.then(db =>
+		new Promise((resolve, reject) => {
+			var myquery = { id: id };
+
+			const col = db.collection('activities');
+				col.deleteOne(myquery, function(err, res){
+					if(err){
+						console.log(err);
+					}
+					resolve(res);
+				});
+		})
+	)
+}
+
+module.exports = Database;
--- server.js
+++ server.js
@@ -1,23 +1,78 @@
-const express = require('express')
-// const mongoose = require('mongoose')
-// const dotenv = require('dotenv').config()
-const app = express()
-const port = 8000
+const express = require("express");
+const Database = require('./Database.js');
 
-// mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
-// const db = mongoose.connection
-// db.on('error', (error) => console.log(error))
-// db.once('open', () => console.log('Connected to database'))
+var db = Database("mongodb+srv://admin:spycatadmin@cluster0.p2llx.mongodb.net/database?retryWrites=true&w=majority", "database");
 
-app.use(express.json())
+const app = express();
+const port = 3000;
 
-const usersRouter = require('./routes/users')
-app.use('/users', usersRouter)
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/users", (req, res) => {
+  console.log("GET /users");
+  db.getUsers()
+    .then(results => {
+      console.log(results);
+      res.status(200).send(JSON.stringify(results));
+    });
+});
+
+// app.post("/activities", (req, res) => {
+//   console.log("Add record!");
+//   let activity = req.body;
+
+//   if (activity) {
+//     db.postActivity(activity)
+//       .then(results => {
+//         console.log(results);
+//         res.status(200).send(JSON.stringify(results))
+//       }).catch(err => {
+//         res.status(400).send(JSON.stringify(err))
+//       });
+//   } else {
+//     res.status(400).send('empty request body found')
+//   }
+// });
+
+// app.put("/activities/:id", (req, res) => {
+//   console.log("Activity changed!");
+//   let id = parseInt(req.params.id);
+//   let activity = req.body;
+
+//   if (activity && id) {
+//     db.putActivity(id, activity)
+//       .then(results => {
+//         console.log(results);
+//         res.status(200).send(JSON.stringify(results))
+//       });
+//   } else {
+//     res.status(400).send('empty request body or param found')
+//   }
+// });
+
+// app.delete("/activities/:id", (req, res) => {
+//   console.log("Activity deleted!");
+//   let id = parseInt(req.params.id);
+
+//   if (id) {
+//     db.deleteActivity(id)
+//       .then(results => {
+//         console.log(results);
+//         res.status(200).send(JSON.stringify(results))
+//       });
+//   } else {
+//     res.status(400).send('empty param found')
+//   }
+// });
 
 app.listen(port, () => {
-  console.log(`node-server listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
+
+module.exports = app;
