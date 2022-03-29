7971d7e1c0d7ac97ed5a96c5b35decd2438cf5c0
stellaw1 <stella01wang@gmail.com>
2022-03-29 15:00:19 -0700

    fix

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = 3000
 
 // mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
 // const db = mongoose.connection
-db.on('error', (error) => console.log(error))
-db.once('open', () => console.log('Connected to database'))
+// db.on('error', (error) => console.log(error))
+// db.once('open', () => console.log('Connected to database'))
 
 app.use(express.json())
 
